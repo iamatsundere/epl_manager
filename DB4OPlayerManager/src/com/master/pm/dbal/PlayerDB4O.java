cecf800ae2a754db6fd2a43423f4d62ac479d95d
Nguyễn Đức Mạnh <ducmanhkthd@gmail.com>
2019-12-12 09:42:38 +0700

    Thực hiện thêm màn hình thêm, sửa, xóa, tìm kiếm + chỉnh sửa lại câu lệnh update + deleteid

--- DB4OPlayerManager/src/com/master/pm/dbal/PlayerDB4O.java
+++ DB4OPlayerManager/src/com/master/pm/dbal/PlayerDB4O.java
@@ -22,72 +22,92 @@ public class PlayerDB4O {
     private static final String DB4OFILENAME = "data/players_db";
 
     public void base() {
-	ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
-	try {
-	} finally {
-	    db.close();
-	}
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
+        try {
+        } finally {
+            db.close();
+        }
     }
 
     public void storePlayer(Player entry) {
-	ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
-	try {
-	    db.store(entry);
-	    System.out.println("Stored " + entry);
-	} finally {
-	    db.close();
-	}
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
+        try {
+            db.store(entry);
+            System.out.println("Stored " + entry);
+        } finally {
+            db.close();
+        }
     }
 
     public void updatePlayerById(Player entry) {
-	ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
-	try {
-	    Player player = new Player();
-	    player.setId(entry.getId());
-	    ObjectSet result = db.queryByExample(player.getId());
-	    Player found = (Player) result.next();
-	    found.setMinutes(found.getMinutes() + entry.getMinutes());
-	    db.store(found);
-	    System.out.println(String.format("Added %d points for %s", entry.getMinutes(), entry.getGoalsScored(), found));
-	} finally {
-	    db.close();
-	}
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
+        try {
+            Player player = new Player();
+            player.setId(entry.getId());
+            ObjectSet result = db.queryByExample(player);
+            Player found = (Player) result.next();
+            found.setMinutes(found.getMinutes() + entry.getMinutes());
+            //update
+            found.setName(entry.getName());
+            found.setCountry(entry.getCountry());
+            found.setClub(entry.getClub());
+            found.setDob(entry.getDob());
+            found.setPos(entry.getPos());
+            found.setPosInfo(entry.getPosInfo());
+            db.store(found);
+            System.out.println(String.format("Added %d points for %s", entry.getMinutes(), entry.getGoalsScored(), found));
+        } finally {
+            db.close();
+        }
     }
 
     public void deletePlayerByName(Player pilot) {
-	ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
-	try {
-	    Player player = new Player();
-	    player.setName(pilot.getName());
-	    ObjectSet result = db.queryByExample(player);
-	    Player found = (Player) result.next();
-	    db.delete(found);
-	} finally {
-	    db.close();
-	}
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
+        try {
+            Player player = new Player();
+            player.setName(pilot.getName());
+            ObjectSet result = db.queryByExample(player);
+            Player found = (Player) result.next();
+            db.delete(found);
+        } finally {
+            db.close();
+        }
+    }
+
+    public void deletePlayerById(Player pilot) {
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
+        try {
+            Player player = new Player();
+            player.setId(pilot.getId());
+            ObjectSet result = db.queryByExample(player);
+            Player found = (Player) result.next();
+            db.delete(found);
+        } finally {
+            db.close();
+        }
     }
 
     public List<Player> listPlayersByQuery(Player query) {
-	ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
-	List<Player> listResult = new ArrayList<>();
-	try {
-	    ObjectSet<Player> queryByExample = db.queryByExample(query);
-	    listResult.addAll(queryByExample);
-	} finally {
-	    db.close();
-	    return listResult;
-	}
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
+        List<Player> listResult = new ArrayList<>();
+        try {
+            ObjectSet<Player> queryByExample = db.queryByExample(query);
+            listResult.addAll(queryByExample);
+        } finally {
+            db.close();
+            return listResult;
+        }
     }
 
     public List<Player> listAllPlayers() {
-	ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
-	List<Player> listResult = new ArrayList<>();
-	try {
-	    ObjectSet<Player> queryByExample = db.query(Player.class);
-	    listResult.addAll(queryByExample);
-	} finally {
-	    db.close();
-	    return listResult;
-	}
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB4OFILENAME);
+        List<Player> listResult = new ArrayList<>();
+        try {
+            ObjectSet<Player> queryByExample = db.query(Player.class);
+            listResult.addAll(queryByExample);
+        } finally {
+            db.close();
+            return listResult;
+        }
     }
 }
